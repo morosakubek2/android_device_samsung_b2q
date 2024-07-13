fc00cf10d0d3f6de728f79cd82613f895772e41e
morosakubek2 <132716550+morosakubek2@users.noreply.github.com>
2024-07-13 21:22:34 +0000

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,6 +26,8 @@ DEVICE_PATH := device/samsung/b2q
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
 
+TARGET_SUPPORTS_64_BIT_APPS := false
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
